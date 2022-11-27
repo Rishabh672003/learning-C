521c6e0a584501c07c4bba70c02dc9d42eed1580
Rishabh <jharishabh2k3@gmail.com>
2022-11-27 12:58:41 +0530

    update

--- while.c
+++ while.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 
 int main() {
-  int x = 0;
-  int y = 0;
-  while (x < 5) {
-    y = x - y;
-    printf("%i%i", x, y);
-    x = x + 1;
-  }
+    int x = 0;
+    int y = 0;
+    while (x < 5) {
+        if (y < 5) {
+            x = x + 1;
+            if (y < 3)
+                x = x - 1;
+        }
+        y = y + 2;
+        printf("%i%i", x, y);
+        x = x + 1;
+    }
 }
