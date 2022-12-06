8cdf9bdef5871f63461c75b116bba13507013655
Rishabh <jharishabh2k3@gmail.com>
2022-12-06 23:24:22 +0530

    fix

--- college_C_stuff/basic.c
+++ college_C_stuff/basic.c
@@ -1,5 +1,5 @@
-#include<stdio.h>
-main(){
-	printf("finding the area of the trangle using the heron's formula")
-	printf("input ")
+#include <stdio.h>
+int main() {
+    printf("finding the area of the trangle using the heron's formula");
+    printf("input ");
 }
